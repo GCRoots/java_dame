99de610871ce52b17017d5b3371ef6ca3d04a1a7
GC <1007879951@qq.com>
2019-09-23 07:38:28 +0800

    init

---
+++ src/java_lock/simpleLockDame.java
@@ -0,0 +1,8 @@
+package java_lock;
+
+/**
+ * @author shipengfei
+ * @data 19-9-23
+ */
+public class simpleLockDame {
+}
---
+++ src/java_sync/simpleSyncDame.java
@@ -0,0 +1,8 @@
+package java_sync;
+
+/**
+ * @author shipengfei
+ * @data 19-9-23
+ */
+public class simpleSyncDame {
+}

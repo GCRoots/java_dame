a413facb01fcb51520455ff2919e10532344b470
GC <1007879951@qq.com>
2019-11-26 23:26:44 +0800

    RabbitMQ simple demo

--- src/main/java/message_queue/README.md
+++ src/main/java/message_queue/README.md
@@ -1,4 +1,4 @@
-#消息队列
+#消息队列 MQ
 
 - ArrayBlockingQueue
 
---
+++ src/main/java/message_queue/RabbitMQ/Consume.java
@@ -0,0 +1,49 @@
+package message_queue.RabbitMQ;
+
+import com.rabbitmq.client.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author shipengfei
+ * @data 19-11-27
+ */
+public class Consume {
+
+    public static void main(String[] args) throws Exception {
+        Consume consume=new Consume();
+        consume.testBasicConsumer();
+    }
+
+    public void testBasicConsumer() throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(AMQP.PROTOCOL.PORT);    // 5672
+        factory.setUsername("mengday");
+        factory.setPassword("mengday");
+
+        // 新建一个长连接
+        Connection connection = factory.newConnection();
+
+        // 创建一个通道(一个轻量级的连接)
+        Channel channel = connection.createChannel();
+
+        // 声明一个队列
+        String QUEUE_NAME = "hello";
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println("Consumer Wating Receive Message");
+
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException, UnsupportedEncodingException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [C] Received '" + message + "'");
+            }
+        };
+
+        // 订阅消息
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
